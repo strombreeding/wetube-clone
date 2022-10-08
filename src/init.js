e1e2a2e5a77a42cc2523b6d87741aa9260ea27d9
strombreeding <speaker1403@gmail.com>
2022-10-09 01:50:28 +0900

    real depoly

--- src/init.js
+++ src/init.js
@@ -8,5 +8,6 @@ require("./db");
 const apiRouter_1 = __importDefault(require("./routers/apiRouter"));
 const server_1 = __importDefault(require("./server"));
 server_1.default.use("/", apiRouter_1.default);
-const PORT = 4000;
-server_1.default.listen(PORT, () => console.log(`✅ Server listening on port ${PORT} 🛸`));
+// const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
+server_1.default.listen(PORT, () => console.log(`✅ ${process.env.PORT} Server listening on port ${PORT} 🛸`));
