2c733b925a7c75e2280e92def0a3792eca02ba94
strombreeding <speaker1403@gmail.com>
2022-12-10 00:34:58 +0900

    자동배포 ㄱㄱ싱

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,12 @@
 import { RequestHandler } from "express";
 import mongoose from "mongoose";
 
-
-mongoose.connect(`${process.env.DB_URL}`)
+mongoose.connect(`${process.env.DB_URL}`);
 
 export const db = mongoose.connection;
 const handleOpen = () => console.log("✅ Connected to DB");
-const handleError:RequestHandler = (error) => console.log("❌ DB Error", error);
+const handleError: RequestHandler = (error) => console.log("❌ DB Error", error);
 
 db.on("error", handleError);
 db.once("open", handleOpen);
+//
